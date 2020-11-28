89679926f6e87189fc670781ac498bf5d2fdc87b
Polaris08 <52858213+Polaris08@users.noreply.github.com>
2020-11-28 19:28:22 +0900

    Directory Check
    Directory Check

--- new_account.py
+++ new_account.py
@@ -1,3 +1,4 @@
+import os
 import cv2
 import numpy as np
 
@@ -5,7 +6,6 @@ def new_account():
     face_classifier = cv2.CascadeClassifier('./templates/haarcascade_frontalface_default.xml')
     
     def face_extractor(img): 
-    
         gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
         faces = face_classifier.detectMultiScale(gray,1.3,5)
     
@@ -16,9 +16,20 @@ def new_account():
             cropped_face = img[y:y+h, x:x+w]
     
         return cropped_face
-    
+        
+       
+    def createFolder(directory):
+        try:
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+        except OSError:
+            print ('Error: Creating directory. ' +  directory)
+
+
     print("Please enter a name to register for a new account.")
     new_account_name = input(); #계정 이름을 사용자에게 입력받음.
+    
+    createFolder('./users/' + new_account_name)
     
     print("Start face recognition. Align your face to the front cam.")
     cap = cv2.VideoCapture(0) #얼굴 인식 시작
@@ -26,13 +37,13 @@ def new_account():
     
     while True: #얼굴이 인식될 때까지 캠 작동
         ret, frame = cap.read() 
-        if face_extractor(frame) is not None: #얼굴이 인식되면 인식된 얼굴을 ./users/[new_account_name].jpg로 저장.
+        if face_extractor(frame) is not None: #얼굴이 인식되면 인식된 얼굴을 저장 (./users/Kim/Kim.jpg)
             count += 1
             print("Face Founded!")
             face = cv2.resize(face_extractor(frame),(200,200))
             face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
     
-            file_name_path = './users/' + new_account_name + '.jpg'
+            file_name_path = './users/' + new_account_name + '/' + new_account_name + '.jpg'
             cv2.imwrite(file_name_path,face)
         else: #얼굴이 인식되지 않으면, 인식될 때까지 계속 진행.
             print("Face not Found")
